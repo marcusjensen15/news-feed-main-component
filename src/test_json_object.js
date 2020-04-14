660eceb74f59ea92fc0d176823028a9faa02c149
Marcus Jensen <marcusjensen1515@gmail.com>
2020-04-14 14:26:19 -0700

    begin building test JSON feed

---
+++ src/test_json_object.js
@@ -0,0 +1,61 @@
+var testJsonObject = [
+    {
+        'newsFeed': 'The Blazers Give Back',
+        'subHeader': 'Charity',
+        'content': 'Jim Jones',
+        'articleLink': 'www.article1.com'
+    },
+    {
+      'firstName': 'Samantha',
+      'lastName': 'Jacobs',
+      'fullName': 'Samantha Jacobs',
+      'role': 'Account Executive',
+      'vacationHoursRemaining':'24',
+      'healthPlanName':'Health Plan C',
+      'startDate':'2019-09-25',
+      'directManager':'Linnea Jones',
+      'email':'samantha@theorem.com',
+      'password':'test2'
+    },
+    {
+      'firstName': 'Bruce',
+      'lastName': 'Stevenson',
+      'fullName': 'Bruce Stevenson',
+      'role': 'Customer Advocate',
+      'vacationHoursRemaining':'17',
+      'healthPlanName':'Health Plan B',
+      'startDate':'2019-11-25',
+      'directManager':'Linnea Jones',
+      'email':'bruce@theorem.com',
+      'password':'test3'
+
+    },
+    {
+      'firstName': 'Linnea',
+      'lastName': 'Jones',
+      'fullName': 'Linnea Jones',
+      'role': 'VP of Sales',
+      'vacationHoursRemaining':'30',
+      'healthPlanName':'Health Plan B',
+      'startDate':'2018-09-25',
+      'directManager':'Toni Braxton',
+      'email':'linnea@theorem.com',
+      'password':'test4'
+
+    },
+    {
+      'firstName': 'Toni',
+      'lastName': 'Braxton',
+      'fullName': 'Toni Braxton',
+      'role': 'CEO',
+      'vacationHoursRemaining':'19',
+      'healthPlanName':'Health Plan A',
+      'startDate':'2018-01-14',
+      'directManager':'',
+      'email':'toni@theorem.com',
+      'password':'test5'
+
+    }]
+    //end testJsonObject
+
+    export default testJsonObject;
